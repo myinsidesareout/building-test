ab8be41063001958a0dfa3762aa9245b3ce4bb06
dldangel <dldxangel@gmail.com>
2022-09-07 23:31:48 -0700

    cycleTile onclick

--- main.js
+++ main.js
@@ -1,36 +1,64 @@
-/* 
-things that need to be done, not necessarily in order:
+const tiles = [];
+var uniqueTiles = 4;
 
-initialize array for all generated divs that contains 
-either tile number or tile name. need to decide, you could
-iterate by name if the png are enumerated. but you can just
-iterate through an array with tile names associated to png
-so i think it makes more sense to go by descriptive tile name.
-wow this is just like minecraft :^)
+var totalTiles = -1;
 
+var rowG = -1;
+var column = 0;
 
+function generateTile() {
 
-*/
+    document.getElementById("test").style.width = 100 * gridColumn + "px";
+    document.getElementById("test").style.height = 100 * gridRow + "px";
 
-function addElement() {
-    var div = document.createElement("div");
-    //div.innerHTML = "Hello";
-    div.className = "tile";
+    for (let row = 0; row < gridRow; row++) {
+        rowG++;
+        for (let column = 0; column < gridColumn; column++) {
+            const div = document.createElement("div");
+            totalTiles++;
+            div.setAttribute("onclick", "cycleTile("+totalTiles+");");
 
-document.getElementById("test").appendChild(div);
-  }
+            var tileId = "tile(" + column + "," + row + ")";
+            // console.log("tileId:" + tileId);
+            div.id = tileId; 
 
-//const column = 1, row = 4;
+            
+            // tiles[totalTiles] = totalTiles;
+            tiles[totalTiles] = tileId;
+            tiles[column][row] = tileId;
+            // console.log("c,r:" + tiles[column,row]);
+            
+            // console.log("tiles[totalTiles]:"+tiles[totalTiles]);
+            
+            div.className = "tile";
 
-function generateTile(column,row) {
+            document.getElementById("test").appendChild(div);
 
-    document.getElementById("test").style.width = 100 * column + "px";
-    document.getElementById("test").style.height = 100 * row + "px";
+            // document.getElementById(tileId).style.backgroundImage = "url(/tile/0.png)";
 
-    for (let tileCount = 0; tileCount < column * row; tileCount++) {
-        addElement();
+        }
     }
-    
 }
 
-generateTile(12,9)
+const gridColumn = 8, gridRow = 4;
+generateTile();
+
+// function cycleTile(column, row) {
+//     console.log(column + "," + row);
+// };
+
+function cycleTile(totalTiles) {
+    var div = document.getElementById(tiles[totalTiles]);
+    // console.log(div);
+    // div.style.backgroundImage = "url(/tile/3.png)";
+    var img = div.style.backgroundImage; 
+    // console.log(img[11]);
+    // console.log("test");
+    var newImg = Number(img.slice(11,-6));
+    if (newImg >= uniqueTiles) { 
+        newImg = 0;
+    } else {newImg +=1};
+    // console.log("help"+newImg);
+    div.style.backgroundImage = "url(/tile/"+ newImg +".png)";
+
+};
